a907deb20a01713906522678d97051af29a6b90a
zh320 <zhanh320@outlook.com>
2023-08-28 20:30:17 +0800

    add ERFNet

---
+++ models/erfnet.py
@@ -0,0 +1,120 @@
+"""
+Paper:      ERFNet: Efficient Residual Factorized ConvNet for Real-Time Semantic Segmentation
+Url:        https://ieeexplore.ieee.org/document/8063438
+Create by:  zh320
+Date:       2023/08/20
+"""
+
+import torch
+import torch.nn as nn
+
+from .modules import ConvBNAct, Activation
+
+
+class ERFNet(nn.Module):
+    def __init__(self, num_class=1, n_channel=3, act_type='relu'):
+        super(ERFNet, self).__init__()
+        self.layer1 = DownsamplerBlock(n_channel, 16, act_type=act_type)
+
+        self.layer2 = DownsamplerBlock(16, 64, act_type=act_type)
+        self.layer3_7 = build_blocks(NonBt1DBlock, 64, 5, act_type=act_type)
+        
+        self.layer8 = DownsamplerBlock(64, 128, act_type=act_type)
+        self.layer9_16 = build_blocks(NonBt1DBlock, 128, 8, 
+                                        dilations=[2,4,8,16,2,4,8,16], act_type=act_type)
+        
+        self.layer17 = Upsample(128, 64, act_type=act_type)
+        self.layer18_19 = build_blocks(NonBt1DBlock, 64, 2, act_type=act_type)
+
+        self.layer20 = Upsample(64, 16, act_type=act_type)
+        self.layer21_22 = build_blocks(NonBt1DBlock, 16, 2, act_type=act_type)
+        
+        self.layer23 = Upsample(16, num_class, act_type=act_type)
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3_7(x)
+        x = self.layer8(x)
+        x = self.layer9_16(x)
+        x = self.layer17(x)
+        x = self.layer18_19(x)
+        x = self.layer20(x)
+        x = self.layer21_22(x)
+        x = self.layer23(x)
+        return x
+
+
+def build_blocks(block, channels, num_block, dilations=[], act_type='relu'):
+    if len(dilations) == 0:
+        dilations = [1 for _ in range(num_block)]
+    else:
+        if len(dilations) != num_block:
+            raise ValueError(f'Number of dilation should be equal to number of blocks')
+
+    layers = []
+    for i in range(num_block):
+        layers.append(block(channels, dilation=dilations[i], act_type=act_type))
+    return  nn.Sequential(*layers)
+    
+    
+class DownsamplerBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, act_type):
+        super(DownsamplerBlock, self).__init__()
+        self.conv = ConvBNAct(in_channels, out_channels - in_channels, 3, 2, act_type=act_type, inplace=True)
+        self.pool = nn.MaxPool2d(3, 2, 1)
+
+    def forward(self, x):
+        x = torch.cat([self.conv(x), self.pool(x)], dim=1)
+        return x
+
+
+class NonBt1DBlock(nn.Module):
+    def __init__(self, channels, dilation=1, act_type='relu'):
+        super(NonBt1DBlock, self).__init__()
+        self.conv = nn.Sequential(
+                                ConvBNAct(channels, channels, (3, 1), inplace=True),
+                                ConvBNAct(channels, channels, (1, 3), inplace=True),
+                                ConvBNAct(channels, channels, (3, 1), dilation=dilation, inplace=True),
+                                nn.Conv2d(channels, channels, (1, 3), dilation=dilation, 
+                                            padding=(0, dilation), bias=False)
+                            )
+        self.bn_act = nn.Sequential(
+                                nn.BatchNorm2d(channels),
+                                Activation(act_type, inplace=True)
+                            )
+
+    def forward(self, x):
+        residual = x
+        x = self.conv(x)
+        x += residual
+        x = self.bn_act(x)
+        return x
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, out_channels, scale_factor=2, kernel_size=None, padding=None,
+                    upsample_type='deconvolution', act_type='relu'):
+        super(Upsample, self).__init__()
+        if upsample_type == 'deconvolution':
+            if kernel_size is None:
+                kernel_size = 2*scale_factor - 1
+            if padding is None:    
+                padding = (kernel_size - 1) // 2
+            output_padding = 2 * padding + 2 - kernel_size
+            self.up_conv = nn.Sequential(
+                                    nn.ConvTranspose2d(in_channels, out_channels, 
+                                                        kernel_size=kernel_size, 
+                                                        stride=scale_factor, padding=padding,
+                                                        output_padding=output_padding),
+                                    nn.BatchNorm2d(out_channels),
+                                    Activation(act_type)
+                            )
+        else:
+            self.up_conv = nn.Sequential(
+                                    ConvBNAct(in_channels, out_channels, 1, act_type=act_type),
+                                    nn.Upsample(scale_factor=scale_factor, mode='bilinear')
+                            )
+
+    def forward(self, x):
+        return self.up_conv(x)
