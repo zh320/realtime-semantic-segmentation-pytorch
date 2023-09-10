fd5b34b2464e3ede222a91eff380cbd95ebc3d7b
zh320 <68283806+zh320@users.noreply.github.com>
2023-09-10 23:11:24 +0800

    add BiSeNetv1
    Former-commit-id: 0c0a1f49f1f5c949d3584d7230bc1f60b31b8f58 [formerly 5c9eec6e812b3b6564aa581d913b186dc4deb503]
    Former-commit-id: 93218ac80591cd693a6f613c5ecb6bfae8bba65e

--- models/__init__.py
+++ models/__init__.py
@@ -13,6 +13,7 @@ from .espnet import ESPNet
 from .erfnet import ERFNet
 from .segnet import SegNet
 from .dabnet import DABNet
+from .bisenetv1 import BiSeNetv1
 
 
 decoder_hub = {'deeplabv3':smp.DeepLabV3, 'deeplabv3p':smp.DeepLabV3Plus, 'fpn':smp.FPN,
@@ -24,7 +25,7 @@ def get_model(config):
     model_hub = {'bisenetv2':BiSeNetv2, 'enet':ENet, 'fastscnn':FastSCNN, 'lednet':LEDNet,
                  'linknet':LinkNet, 'contextnet':ContextNet, 'ppliteseg':PPLiteSeg,
                  'ddrnet':DDRNet, 'espnet':ESPNet, 'erfnet':ERFNet, 'segnet':SegNet,
-                 'dabnet':DABNet,}
+                 'dabnet':DABNet, 'bisenetv1':BiSeNetv1,}
 
     # The following models currently support auxiliary heads
     aux_models = ['bisenetv2', 'contextnet', 'fastscnn', 'ddrnet']
